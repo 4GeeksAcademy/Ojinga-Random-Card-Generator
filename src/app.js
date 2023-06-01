c3ce2679c93c1e0ab7cd3c8208838d8e5ada71ce
GISOjinga <103523730+GISOjinga@users.noreply.github.com>
2023-06-01 18:23:34 +0000

    Done

--- src/app.js
+++ src/app.js
@@ -6,12 +6,37 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  let cards = ["K", "Q", "J", "A"];
+  let cardValues = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "K",
+    "Q",
+    "J",
+    "A"
+  ];
 
-  // the numbers
-  let number = Math.random(2, 10);
+  let cardSuits = ["♦", "♣", "♠", "♥"];
 
-  // the icons
-  let cardIcons = ["♦", "♣", "♠", "♥"];
+  // Randomly select a card value
+  let cardValue = cardValues[Math.floor(Math.random() * cardValues.length)];
+
+  // Randomly select a card suit
+  let cardSuit = cardSuits[Math.floor(Math.random() * cardSuits.length)];
+
+  // Get the card element
+  let cardElement = document.querySelector(".card");
+
+  // Update the card element's HTML to display the card value and suit
+  cardElement.innerHTML = `
+    <span class="card-suit top-left">${cardSuit}</span>
+    <span class="card-value">${cardValue}</span>
+    <span class="card-suit bottom-right">${cardSuit}</span>
+  `;
 };
